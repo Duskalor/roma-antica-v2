21c62db5e1ae66b0a1db1d2fd9b5716f412f2c97
duskalor <duskalor1@gmail.com>
2024-10-24 01:34:19 -0500

    ft:home apge

---
+++ src/lib/const.ts
@@ -0,0 +1,48 @@
+import slide01 from '@Images/carrousel/1.jpg';
+import slide02 from '@Images/carrousel/2.jpg';
+import slide03 from '@Images/carrousel/3.jpg';
+import slide04 from '@Images/carrousel/4.jpg';
+import slide05 from '@Images/carrousel/5.jpg';
+import otro from '@Images/otro.jpg';
+
+export const imagesCarrousel = [
+  { image: slide01, name: 'image1' },
+  { image: slide02, name: 'image2' },
+  { image: slide03, name: 'image3' },
+  { image: slide04, name: 'image4' },
+  { image: slide05, name: 'image5' },
+];
+
+export const navLinks = [
+  { href: '/', label: 'Inicio' },
+  { href: '/nosotros', label: 'Nosotros' },
+  { href: '/carta', label: 'Carta' },
+  { href: '/contacto', label: 'Contacto' },
+];
+
+export const ofertas = [
+  {
+    title: 'PIZZA XL',
+    description:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    aparte:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    image: otro,
+  },
+  {
+    title: 'PIZZA XL',
+    description:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    aparte:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    image: otro,
+  },
+  {
+    title: 'PIZZA XL',
+    description:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    aparte:
+      'La pizza es uno de los platos más icónicos y apreciados a nivel mundial.',
+    image: otro,
+  },
+];
