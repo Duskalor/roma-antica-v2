fa020326be8618bce269f1468e6e1cf53e1e7688
Dinamico-xD <milton051091@gmail.com>
2024-10-25 01:42:53 -0500

    logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        lucuma:"#FFBD52", lila:"#7A1315"
+      }
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
