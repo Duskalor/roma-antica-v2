990074f8c20de30b6915ef967b7499a98c024079
duskalor <duskalor1@gmail.com>
2024-10-29 21:50:50 -0500

    ft:things

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,12 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors :{
-        lucuma:"#FFBD52", lila:"#7A1315"
-      }
+      colors: {
+        lucuma: '#FFBD52',
+        lila: '#7A1315',
+        verde: '#345C2F',
+        anaranjado: '#CB2128',
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
